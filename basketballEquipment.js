3a005349f3d80134bc7b94e6360fe69740a8a2b9
Emil Evtimov <eevtimov176@gmail.com>
2025-01-05 23:01:29 +0200

    Add files via upload

---
+++ basketballEquipment.js
@@ -0,0 +1,11 @@
+function basketballEquipment(yearlyTrainingPrice) {
+    let basketballShoes = yearlyTrainingPrice * 60 / 100;
+    let basketballKit = basketballShoes * 80 / 100;
+    let basketBall = basketballKit / 4;
+    let accessories = basketBall / 5;
+    let expenses = yearlyTrainingPrice + basketballShoes + basketballKit + basketBall + accessories;
+    console.log(`${expenses}`)
+}
+
+
+basketballEquipment(365)
