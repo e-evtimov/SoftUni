87c97bb8b4c135225439f49cf40c621614363f36
Emil Evtimov <eevtimov176@gmail.com>
2025-01-05 23:04:20 +0200

    Add files via upload

---
+++ petFoodPrice.js
@@ -0,0 +1,7 @@
+function expenses(dogFood, catFood) {
+    let dogFoodPrice = dogFood * 2.50;
+    let catFoodPrice = catFood * 4;
+    let result = dogFoodPrice + catFoodPrice
+        console.log(`"${result} lv"`)
+}
+
